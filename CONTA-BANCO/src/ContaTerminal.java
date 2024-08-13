50e25b1e012a613282da4dd5e352955c1c08cde7
Reinaldo Alves de Oliveira <reinaldoo.iot@gmail.com>
2024-08-13 15:03:44 -0400

    Update

--- CONTA-BANCO/src/ContaTerminal.java
+++ CONTA-BANCO/src/ContaTerminal.java
@@ -2,28 +2,25 @@ import java.util.Scanner;
 
 public class ContaTerminal {
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
-        String i, nome, agencia;
-        int numero;
+
+        System.out.println("Digite o Número da Agência: ");
+        String agencia = scanner.next();
+
+        System.out.println("Digite o Número da Conta: ");
+        int numero = scanner.nextInt();
+
+        System.out.println("Digite Nome do Cliente: ");
+        String nome = scanner.next();
+
+        System.out.println("Valor Depositado: ");
         double saldo;
-        if (i.equals("sim")) {
-            System.out.println("Digite o Número da Agência: ");
-            agencia = scanner.next();
+        saldo = scanner.nextDouble();
 
-            System.out.println("Digite o Número da Conta: ");
-            numero = scanner.nextInt();
+        System.out.println("Olá "+ nome +", obrigado por criar uma conta em nosso banco, sua agência é "+ agencia +", " +
+                "conta " + numero + " e seu saldo "+ saldo + " já está disponível para saque");
 
-            System.out.println("Digite Nome do Cliente: ");
-             nome = scanner.next();
-
-            System.out.println("Valor Depositado: ");
-             saldo = scanner.nextDouble();
-
-
-        }else{
-            System.out.println("Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", " +
-                    "conta " + numero + " e seu saldo " + saldo + " já está disponível para saque");
-        }
         
     }
 }
