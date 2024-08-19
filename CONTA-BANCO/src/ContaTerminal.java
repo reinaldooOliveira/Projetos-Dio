a370a5b18c67ea7ab1a5e12a51b23bba8f434826
Reinaldo Alves de Oliveira <reinaldoo.iot@gmail.com>
2024-08-19 09:38:51 -0400

    update novo metodo excecoes

--- CONTA-BANCO/src/ContaTerminal.java
+++ CONTA-BANCO/src/ContaTerminal.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class ContaTerminal {
@@ -5,21 +6,29 @@ public class ContaTerminal {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Digite o Número da Agência: ");
-        String agencia = scanner.next();
+        try {
+            System.out.println("Digite o Número da Agência: ");
+            String agencia = scanner.next();
 
-        System.out.println("Digite o Número da Conta: ");
-        int numero = scanner.nextInt();
+            System.out.println("Digite o Número da Conta: ");
+            int numero = scanner.nextInt();
 
-        System.out.println("Digite Nome do Cliente: ");
-        String nome = scanner.next();
+            System.out.println("Digite Nome do Cliente: ");
+            String nome = scanner.next();
 
-        System.out.println("Valor Depositado: ");
-        double saldo;
-        saldo = scanner.nextDouble();
+            System.out.println("Valor Depositado: ");
+            double saldo;
+            saldo = scanner.nextDouble();
 
-        System.out.println("Olá "+ nome +", obrigado por criar uma conta em nosso banco, sua agência é "+ agencia +", " +
-                "conta " + numero + " e seu saldo "+ saldo + " já está disponível para saque");
+            System.out.println("Olá "+ nome +", obrigado por criar uma conta em nosso banco, sua agência é "+ agencia +", " +
+                    "conta " + numero + " e seu saldo "+ saldo + " já está disponível para saque");
+
+        }
+        catch ( InputMismatchException e){
+
+            System.out.println("Favor Verificar, dados adicionado incorretamente !");
+        }
+
 
         
     }
